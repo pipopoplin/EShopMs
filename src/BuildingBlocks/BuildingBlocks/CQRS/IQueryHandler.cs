7e57a7792cd31ca4cd93c9ce9ec7e8285815274e
Shahead <dauerblender@gmx.net>
2024-12-22 13:02:14 +0100

    implement CQRS handlers in BuildingBlocks

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+public interface IQueryHandler<in TQuery, TResponce>
+    : IRequestHandler<TQuery, TResponce>
+    where TQuery : ICommand<TResponce>
+    where TResponce : notnull
+{
+
+}
