676d5adf6a79363cba964a7ff7c585625185d9e6
Shahead <dauerblender@gmx.net>
2024-12-24 09:39:50 +0100

    configure pg marten object db

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -8,6 +8,11 @@ builder.Services.AddMediatR(conf =>
     conf.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
 
+builder.Services.AddMarten(opt =>
+{
+    opt.Connection(builder.Configuration.GetConnectionString("DataBase")!);
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
 // configure http request pipeline
