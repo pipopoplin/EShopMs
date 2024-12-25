66322ad455ddf4c588205a505605e5f1f55b54a3
Shahead <dauerblender@gmx.net>
2024-12-25 09:19:39 +0100

    add UpdateProduct

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndPoint.cs
@@ -0,0 +1,30 @@
+﻿using Mapster;
+
+namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductRequest(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+
+public record UpdateProductResponce(bool IsSuccess);
+
+public class UpdateProductEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/products", async (UpdateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<UpdateProductCommand>();
+
+            var result = await sender.Send(command);
+
+            var responce = result.Adapt<UpdateProductResponce>();
+
+            return Results.Ok(responce);
+        })
+        .WithName(nameof(UpdateProductEndPoint))
+        .Produces<UpdateProductResponce>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Update Product")
+        .WithDescription("Update Product");
+    }
+}
