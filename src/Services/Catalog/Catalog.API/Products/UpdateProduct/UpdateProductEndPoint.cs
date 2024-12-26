f92cf2bc724bae1419c831b43f540d4359cd86f1
Shahead <dauerblender@gmx.net>
2024-12-26 09:19:18 +0100

    add validation for command handlers

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndPoint.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndPoint.cs
@@ -1,6 +1,4 @@
-﻿using Mapster;
-
-namespace Catalog.API.Products.UpdateProduct;
+﻿namespace Catalog.API.Products.UpdateProduct;
 
 public record UpdateProductRequest(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price);
 
