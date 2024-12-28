f80f403a7e517a2caa04f98b83b6672f4e09bc6e
Shahead <dauerblender@gmx.net>
2024-12-28 17:01:37 +0100

    add StoreBasket Endpoint and Handler

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketCommand(ShopingCart Cart) : ICommand<StoreBasketResult>;
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketCommandValidiator : AbstractValidator<StoreBasketCommand>
+{
+    public StoreBasketCommandValidiator()
+    {
+        RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+        RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("Cart.UserName is required");
+    }
+}
+
+public class StoreBasketCommandHandler() : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        ShopingCart cart = command.Cart;
+
+        return new StoreBasketResult("Test");
+    }
+}
--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndPoints.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndPoints.cs
@@ -1,5 +1,27 @@
 ﻿namespace Basket.API.Basket.StoreBasket;
 
-public class StoreBasketEndPoints
+public record StoreBasketRequest(ShopingCart Cart);
+
+public record StoreBasketResponse(string UserName);
+
+public class StoreBasketEndPoints : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<StoreBasketCommand>();
+
+            var result = await sender.Send(command);
+
+            var responce = result.Adapt<StoreBasketResponse>();
+
+            return Results.Created($"/basket/{responce.UserName}", responce);
+        })
+        .WithName("StoreBasket")
+        .Produces<StoreBasketEndPoints>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Store Basket")
+        .WithDescription("Store Basket");
+    }
 }
