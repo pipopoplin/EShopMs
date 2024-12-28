9e614dfedc50ee5a5b13c8f66ea98eea0a505a2f
Shahead <dauerblender@gmx.net>
2024-12-28 09:59:09 +0100

    add HealthChecks for Postgres

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,33 +1,45 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services
 var assembly = typeof(Program).Assembly;
 
-builder.Services.AddMediatR(conf =>
+var services = builder.Services;
+
+services.AddMediatR(conf =>
 {
     conf.RegisterServicesFromAssembly(assembly);
     conf.AddOpenBehavior(typeof(VailidationBehavior<,>));
     conf.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
 
-builder.Services.AddValidatorsFromAssembly(assembly);
+services.AddValidatorsFromAssembly(assembly);
 
-builder.Services.AddCarter();
+services.AddCarter();
 
-builder.Services.AddMarten(opt =>
+services.AddMarten(opt =>
 {
     opt.Connection(builder.Configuration.GetConnectionString("DataBase")!);
 }).UseLightweightSessions();
 
 if(builder.Environment.IsDevelopment())
-    builder.Services.InitializeMartenWith<CatalogInitialData>();
+    services.InitializeMartenWith<CatalogInitialData>();
 
-builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+services.AddExceptionHandler<CustomExceptionHandler>();
+
+services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("DataBase")!);
 
 var app = builder.Build();
 
 // configure http request pipeline
 app.MapCarter();
+
 app.UseExceptionHandler(options => { });
+
+app.MapHealthChecks("/health", 
+    new HealthCheckOptions { ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse });
 
 app.Run();
