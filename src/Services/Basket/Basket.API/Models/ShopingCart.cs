cbb19d43bc58b5bb78a4f200586061b798275a0e
Shahead <dauerblender@gmx.net>
2024-12-28 15:43:32 +0100

    add ShopingCart model

---
+++ src/Services/Basket/Basket.API/Models/ShopingCart.cs
@@ -0,0 +1,20 @@
+﻿namespace Basket.API.Models;
+
+public class ShopingCart
+{
+    public string UserName { get; set; } = default!;
+    public List<ShopingCartItem> Items { get; set; } = default!;
+
+    public decimal TotalPrice => Items.Sum(i => i.Price * i.Quantity);
+
+    public ShopingCart(string userName)
+    {
+        UserName = userName;
+    }
+
+    // required for mapping
+    public ShopingCart()
+    {
+        
+    }
+}
