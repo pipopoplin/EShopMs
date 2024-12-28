a6505ad1254c1ea5d9b921a90f7265e5ab54f792
Shahead <dauerblender@gmx.net>
2024-12-28 16:36:09 +0100

    add GetBasket Endpoint and Handler

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+public record GetBasketResult(ShopingCart Cart);
+
+public class GetBasketQueryHandler(IDocumentSession session) 
+    : IQueryHandler<GetBasketQuery, GetBasketResult>
+{
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        // TODO: implement repository
+
+        return new GetBasketResult(new ShopingCart("Test"));
+    }
+}
---
+++ src/Services/Basket/Basket.API/Exceptions/BasketNotFoundException.cs
@@ -0,0 +1,6 @@
+﻿namespace Basket.API.Exceptions;
+
+public class BasketNotFoundException : NotFoundException
+{
+    public BasketNotFoundException(string UserName) : base("Basket", UserName) { }
+}
