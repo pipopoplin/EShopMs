f9a724c78d4a8d8560ea1eaba2d08f6f69caab53
Shahead <dauerblender@gmx.net>
2024-12-30 10:02:24 +0100

    add repository pattern

---
+++ src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace Basket.API.Data;
+
+public class BasketRepository(IDocumentSession session) : IBasketRepository
+{
+    public async Task<ShopingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        var basket = await session.LoadAsync<ShopingCart>(userName, cancellationToken);
+        return basket ?? throw new BasketNotFoundException(userName);
+    }
+
+    public async Task<ShopingCart> StoreBasket(ShopingCart basket, CancellationToken cancellationToken = default)
+    {
+        session.Store(basket);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return basket;
+    }
+
+    public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        session.Delete<ShopingCart>(userName);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return true;
+    }
+}
