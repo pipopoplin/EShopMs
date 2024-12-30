cd27364d54a048cc1e03848ae63c306064b37cee
Shahead <dauerblender@gmx.net>
2024-12-30 10:51:57 +0100

    Basket add exception handling and health check

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,6 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var assembly = typeof(Program).Assembly;
@@ -20,10 +23,20 @@ services.AddMarten(opt =>
 
 services.AddScoped<IBasketRepository, BasketRepository>();
 
+services.AddExceptionHandler<CustomExceptionHandler>();
+
+services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("DataBase")!);
+
 var app = builder.Build();
 
 // configure http request pipeline
 app.MapCarter();
+
+app.UseExceptionHandler(options => { });
+
+app.MapHealthChecks("/health",
+    new HealthCheckOptions { ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse });
 
 app.Run();
 
