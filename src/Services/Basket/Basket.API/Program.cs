d59d3cfb66c64f4c6ea6082c556f0447bb34d512
Shahead <dauerblender@gmx.net>
2024-12-30 09:18:54 +0100

    add Carter and MadiatR services

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,7 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var assembly = typeof(Program).Assembly;
+var services = builder.Services;
+
 // Add services
+services.AddCarter();
+services.AddMediatR(conf =>
+{
+    conf.RegisterServicesFromAssembly(assembly);
+    conf.AddOpenBehavior(typeof(VailidationBehavior<,>));
+    conf.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
 
 var app = builder.Build();
+
 // configure http request pipeline
+app.MapCarter();
 
 app.Run();
+
