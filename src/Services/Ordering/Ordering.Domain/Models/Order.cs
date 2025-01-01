2d42b7626791f69f7ac4f7f329a6d818310e0df1
Shahead <dauerblender@gmx.net>
2025-01-01 15:22:33 +0100

    add ordering domain model

---
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿namespace Ordering.Domain.Models;
+
+public class Order : Aggregate<Guid>
+{
+    public List<OrderItem> _orderItems = new();
+    public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
+
+    public Guid CustomerId { get; private set; } = default!;
+    public string OrderName { get; private set; } = default!;
+    public Address ShippingAddress { get; private set; } = default!;
+    public Address BillingAddress { get; private set; } = default!;
+    public OrderStatus Payment { get; private set; } = default!;
+    public string Status { get; private set; } = default!;
+    public decimal TotalPrice
+    {
+        get => OrderItems.Sum(x => x.Price * x.Quantity);
+        private set { }
+    }
+
+
+
+
+
+
+}
---
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace Ordering.Domain.Models;
+public class Product : Entity<Guid>
+{
+    public string Name { get; private set; } = default!;
+    public decimal Price { get; private set; } = default!;
+
+    public static Product Create(string name, decimal price)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var product = new Product
+        {
+            Name = name,
+            Price = price
+        };
+
+        return product;
+    }
+}
