c11d7315ea624ba86b639cc738b16aee45bd26ca
Shahead <dauerblender@gmx.net>
2025-01-02 09:50:26 +0100

    add db initial migration

--- src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
@@ -7,9 +7,10 @@ public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
     public void Configure(EntityTypeBuilder<OrderItem> builder)
     {
         builder.HasKey(oi => oi.Id);
-        builder.Property(oi => oi.Id).HasConversion(
-                                   orderItemId => orderItemId.Value,
-                                   dbId => OrderItemId.Of(dbId));
+        builder.Property(oi => oi.Id)
+            .HasConversion(
+                orderItemId => orderItemId.Value,
+                dbId => OrderItemId.Of(dbId));
 
         builder.HasOne<Product>()
             .WithMany()
