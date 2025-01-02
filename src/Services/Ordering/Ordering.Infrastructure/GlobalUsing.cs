b90a2d833ef60419fee1fb2c1497c59d143684e5
Shahead <dauerblender@gmx.net>
2025-01-02 09:31:26 +0100

    configure DI for Infrastructure

--- src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -1,3 +1,6 @@
 ﻿global using Microsoft.EntityFrameworkCore;
 global using Ordering.Domain.Models;
 global using System.Reflection;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Ordering.Infrastructure.Data;
