7467cfd1c91c17f8f1d5db7df8e10a7e8a0db19e
Shahead <dauerblender@gmx.net>
2025-01-05 14:35:55 +0100

    add Order Domain Events

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Orders.Application.Orders.EventHandlers.Domain;
+
+public class OrderCreatedEventHandler
+    //(IPublishEndpoint publishEndpoint, IFeatureManager featureManager,
+    (ILogger<OrderCreatedEventHandler> logger)
+    : INotificationHandler<OrderCreatedEvent>
+{
+    public Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
+
+        //if (await featureManager.IsEnabledAsync("OrderFullfilment"))
+        //{
+        //    var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
+        //    await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+        //}
+
+        return Task.CompletedTask;
+    }
+}
