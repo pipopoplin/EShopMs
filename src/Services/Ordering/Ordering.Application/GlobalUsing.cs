6afeaf20dc8c7acd7d71121bffa19f4f3ce56f33
Shahead <dauerblender@gmx.net>
2025-01-05 09:45:18 +0100

    add boilerplate and folder structure

---
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
+global using BuildingBlocks.CQRS;
+global using MediatR;
+global using Microsoft.Extensions.Logging;
+global using Ordering.Domain.Events;
