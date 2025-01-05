e0b2a55351b5d2cc2947b36e26fae112046481e1
Shahead <dauerblender@gmx.net>
2025-01-05 10:10:26 +0100

    add CreateOrderCommand section

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -4,3 +4,6 @@ global using BuildingBlocks.CQRS;
 global using MediatR;
 global using Microsoft.Extensions.Logging;
 global using Ordering.Domain.Events;
+
+global using FluentValidation;
+global using Ordering.Application.Dtos;
---
+++ src/Services/Ordering/Ordering.Application/Ordering/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Ordering.Application.Ordering.Orders.Commands.CreateOrder;
+
+public class CreateOrderCommandHandler :
+    ICommandHandler<CreateOrderCommand, CreateOrderResult>
+{
+    public Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
